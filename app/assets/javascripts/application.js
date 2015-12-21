9a45e5aa7ab8460cb82e93a240972a8f698d9734
Bejan Maljai <bmaljai@gmail.com>
2015-12-20 16:45:28 -0800

    initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,20 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require theme/bootstrap.min
+//= require theme/plugins/morris/raphael.min.js
+//= require theme/plugins/morris/morris.min.js
+//= require theme/plugins/morris/morris-data.js
+//= require_tree .
