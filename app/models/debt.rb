700ab0ba8a362f9b07587ba4b10c5d06b3f4a228
Bejan Maljai <bmaljai@gmail.com>
2016-01-10 12:01:06 -0800

    added debt model method and updated invoice show page

--- app/models/debt.rb
+++ app/models/debt.rb
@@ -1,4 +1,16 @@
 class Debt < ActiveRecord::Base
   belongs_to :invoice
   belongs_to :user
+
+  def outstanding_balance
+    amnt = 0
+    pd_amount = 0
+   
+    (amnt = amount) if amount != nil
+    (pd_amount = paid_amount) if paid_amount != nil
+      
+    return amnt - pd_amount
+    
+  end
+
 end
