f6eea374a310d11789cff2d52e1d0d728be42f79
Bejan Maljai <bmaljai@gmail.com>
2016-01-17 08:59:44 -0800

    added action mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,6 @@ class UserMailer < ApplicationMailer
   default from: 'notifications@payup.com'
 
   def welcome_email(email)
-    @user = user
     @url = "http://localhost:3000/users/sign_in"
     mail(to: email, subject: 'Welcome to PayUp!')
   end
